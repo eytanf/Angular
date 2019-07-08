c00de3ef5611cabb4869dfe8e0e1991af2cd17a3
eytanf <eytanfitoussi@gmail.com>
2019-07-08 18:55:27 +0300

    any

---
+++ hello-world/src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test]',
+  template: 
+  `<p>
+    Fuck you Elad i took the welcome down! nub gitGud!
+  </p>`,
+  styleUrls: []
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
